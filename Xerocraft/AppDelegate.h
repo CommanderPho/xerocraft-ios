810ba7b79789247aef28116bf40e89a242f9c57b
Adrian Boyko <AdrianBoyko@gmail.com>
2015-06-22 13:49:00 -0700

    Initial Commit

---
+++ Xerocraft/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xerocraft
+//
+//  Created by Adrian Boyko on 6/22/15.
+//  Copyright (c) 2015 Adrian Boyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xerocraft/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xerocraft
+//
+//  Created by Adrian Boyko on 6/22/15.
+//  Copyright (c) 2015 Adrian Boyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
